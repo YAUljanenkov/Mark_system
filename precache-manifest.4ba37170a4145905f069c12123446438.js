dc4542065d8c6944fb67459b596bcf0f8bb47389
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2019-03-06 11:14:17 +0300

    Updates

---
+++ precache-manifest.4ba37170a4145905f069c12123446438.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69ec90a999ca5744dc6c",
+    "url": "/YAUljanenkov/Mark_system/static/js/runtime~main.69ec90a9.js"
+  },
+  {
+    "revision": "9dbd6ea4083a41efa783",
+    "url": "/YAUljanenkov/Mark_system/static/js/main.9dbd6ea4.chunk.js"
+  },
+  {
+    "revision": "acde8fad22a4777a5f34",
+    "url": "/YAUljanenkov/Mark_system/static/js/1.acde8fad.chunk.js"
+  },
+  {
+    "revision": "9dbd6ea4083a41efa783",
+    "url": "/YAUljanenkov/Mark_system/static/css/main.096e69b6.chunk.css"
+  },
+  {
+    "revision": "9b727d9b6a34d86c9cb9b8b77ca290b0",
+    "url": "/YAUljanenkov/Mark_system/index.html"
+  }
+];
